272f89fc4dd53329e7964398f82641b2ec99e404
Conanas <tmhaigh@gmail.com>
2020-10-25 09:43:54 +1100

    view functions working properly

--- assets/js/view.js
+++ assets/js/view.js
@@ -1,3 +1,120 @@
 const inquirer = require("inquirer");
+const cTable = require("console.table");
 
 const indexModule = require("../../index");
+const CRUD = require("./CRUD");
+
+module.exports = {
+
+    viewDepartments: async function() {
+        try {
+            let departments = await CRUD.getDepartments();
+            console.log("\n");
+            const table = cTable.getTable(departments);
+            console.log(table);
+            indexModule.initiate();
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+    viewRoles: async function() {
+        try {
+            let roles = await CRUD.getRoles();
+            console.log("\n");
+            const table = cTable.getTable(roles);
+            console.log(table);
+            indexModule.initiate();
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+    viewEmployees: async function() {
+        try {
+            let employees = await CRUD.getEmployees();
+            console.log("\n");
+            const table = cTable.getTable(employees);
+            console.log(table);
+            indexModule.initiate();
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+    viewEmployeesByManagerPrompts: function(managers) {
+        return inquirer.prompt([{
+            type: "rawlist",
+            message: "Select manager",
+            choices: managers.map(manager => `${manager.first_name} ${manager.last_name}`),
+            name: "manager"
+        }])
+    },
+
+    viewEmployeesByManager: async function() {
+        try {
+            let managers = await CRUD.getManagers();
+            let answers = await this.viewEmployeesByManagerPrompts(managers);
+            answersSplit = answers.manager.split(" ");
+            managers.forEach(manager => {
+                if (manager.first_name === answersSplit[0] && manager.last_name === answersSplit[1]) {
+                    answers.manager = manager.id;
+                    return;
+                }
+            })
+            let employeesByManager = await CRUD.getEmployeesByManager(answers);
+            if (employeesByManager.length === 0) {
+                console.log("\nNo employees under this manager\n");
+            } else {
+                console.log("\n");
+                const table = cTable.getTable(employeesByManager);
+                console.log(table);
+            }
+            indexModule.initiate();
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+    viewSelectionSwitch(viewSelection) {
+        switch (viewSelection.viewSelection) {
+            case ("Departments"):
+                this.viewDepartments();
+                break;
+            case ("Roles"):
+                this.viewRoles();
+                break;
+            case ("Employees"):
+                this.viewEmployees();
+                break;
+            case ("Employees by manager"):
+                this.viewEmployeesByManager();
+                break;
+            default:
+                console.log("View Switch Error");
+        }
+    },
+
+    viewSelectionPrompts: function() {
+        return inquirer.prompt([{
+            type: "rawlist",
+            message: "What would you like to view?",
+            choices: [
+                "Departments",
+                "Roles",
+                "Employees",
+                "Employees by manager"
+            ],
+            name: "viewSelection"
+        }])
+    },
+
+    viewDatabase: async function() {
+        try {
+            let viewSelection = await this.viewSelectionPrompts();
+            this.viewSelectionSwitch(viewSelection);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
