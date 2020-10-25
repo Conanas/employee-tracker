e081fd0ea6e4b846b4e0f7fcd6733df8f2a49767
Conanas <tmhaigh@gmail.com>
2020-10-26 10:29:57 +1100

    removed select queries

--- assets/seed.sql
+++ assets/seed.sql
@@ -3,8 +3,6 @@ USE employee_db;
 INSERT INTO department (name)
 VALUES ("Sales"), ("Warehouse"), ("Marketing");
 
-SELECT * FROM department;
-
 INSERT INTO role (title, salary, department_id)
 VALUES
 ("Sales Manager", 52000, 1),
@@ -14,15 +12,11 @@ VALUES
 ("Marketing Manager", 60000, 3),
 ("Marketing Assistant", 56000, 3);
 
-SELECT * FROM role;
-
 INSERT INTO employee (first_name, last_name, role_id)
 VALUES
 ("Tom", "Haigh", 1),
 ("Stevie", "Wunderbar", 3),
 ("Virginia", "Were-woolf", 5);
-
-SELECT * FROM employee;
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
@@ -32,5 +26,3 @@ VALUES
 ("Joanna", "Plumley", 4, 3),
 ("Angela", "Lambsbury", 6, 5),
 ("Douglas", "Fir", 6, 5);
-
-SELECT * FROM employee;
