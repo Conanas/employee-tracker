fcaf15e436b0cec50bee37aba619076743c1dbdb
Conanas <tmhaigh@gmail.com>
2020-10-25 12:12:09 +1100

    added modules and exports

--- assets/js/delete.js
+++ assets/js/delete.js
@@ -1,3 +1,9 @@
 const inquirer = require("inquirer");
+const cTable = require("console.table");
 
 const indexModule = require("../../index");
+const CRUD = require("./CRUD");
+
+module.exports = {
+
+}
