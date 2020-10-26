42a74759206ebac75076a7c60192d1d1e6bbc7a3
Conanas <tmhaigh@gmail.com>
2020-10-27 10:24:02 +1100

    added comments

--- assets/js/budget.js
+++ assets/js/budget.js
@@ -1,26 +1,14 @@
+// required node modules
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 
+// required local modules
 const indexModule = require("../../index");
 const CRUD = require("./CRUD");
 
 module.exports = {
 
-    viewBudgets: async function() {
-        try {
-            let departments = await CRUD.getDepartments();
-            let answers = await this.viewDepartmentsPrompts(departments);
-            answers.id = answers.department.split(" ")[1];
-            let budgetByDepartment = await CRUD.viewBudgetByDepartment(answers.id);
-            console.log("\n");
-            const table = cTable.getTable(budgetByDepartment);
-            console.log(table);
-            indexModule.initiate();
-        } catch (error) {
-            console.log(error);
-        }
-    },
-
+    // prompts the user to select a department
     viewDepartmentsPrompts: function(departments) {
         return inquirer.prompt([{
             type: "list",
@@ -28,5 +16,32 @@ module.exports = {
             choices: departments.map(department => `id: ${department.id} name: ${department.name}`),
             name: "department"
         }])
+    },
+
+    // starts the functions required to view the utilised budget of a department
+    viewBudgets: async function() {
+        try {
+            // retrieves teh departments from teh database
+            let departments = await CRUD.getDepartments();
+
+            // retrieves the selected department to view the budget
+            let answers = await this.viewDepartmentsPrompts(departments);
+
+            // retrieves and stores the department id from the answers
+            answers.id = answers.department.split(" ")[1];
+
+            // retrieves the budget of the chosen department
+            let budgetByDepartment = await CRUD.viewBudgetByDepartment(answers.id);
+
+            // displays the budget spent to the console
+            console.log("\n");
+            const table = cTable.getTable(budgetByDepartment);
+            console.log(table);
+
+            // back to main menu
+            indexModule.initiate();
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
